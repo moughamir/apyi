c1f552a5a0799ee596b9ff90b5c3695f7dc2c1cf
Mohamed Moughamir <moughamir@gmail.com>
2020-09-05 18:51:25 +0100

    Add Tasks model

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_NAME'] = 'sqlite:///holder.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///holder.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+# Model
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(200), nullable=False)
+  completed = db.Column(db.Integer, default=0)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+  def __repr__(self):
+    return '<Task %r>' % self.id
+
+# Routes
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+  if request.method == 'POST':
+    task_content = request.form['content']
+    new_task = Todo(content=task_content)
+
+    try:
+      db.session.add(new_task)
+      db.session.commit()
+      return redirect('/')
+    except:
+      return "Issue while adding a task"
+
+  else:
+    tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html', tasks=tasks)
+
+# Runner
 if __name__ == "__main__":
   app.run(debug=True)
