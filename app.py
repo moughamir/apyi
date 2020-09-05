4ff1dfdda196750537c7789fbacb6d88fc185ffa
Mohamed Moughamir <moughamir@gmail.com>
2020-09-05 17:55:41 +0100

    dbinitiate

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_NAME'] = 'sqlite:///holder.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
